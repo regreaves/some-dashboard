a5a5344e6e9b28a97876e9e91e4486159123461e
Ralph Greaves <rgreaves@ycp.edu>
2023-10-21 23:01:48 -0400

    Add files via upload

---
+++ app.mjs
@@ -0,0 +1,35 @@
+import 'dotenv/config';
+
+import express from 'express';
+
+import pkg from 'express-openid-connect';
+const { auth, requiresAuth } = pkg;
+
+const app = express();
+
+app.set('view engine', 'pug');
+
+app.use(auth({
+  authorizationParams: {
+    response_type: 'code'
+  },
+  authRequired: false
+}));
+
+app.use((req, res, next) => {
+  res.locals.user = req.oidc.user;
+
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.status(200).render('index');
+});
+
+app.get('/account', requiresAuth(), (req, res) => {
+  res.status(200).render('account');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}...`);
+});
